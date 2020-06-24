cb82b1ec70a3419eaa617d7599fe9b3b102d3b83
Suprateek Chatterjee <suprateek1912@gmail.com>
2020-06-24 08:00:21 +0000

    Added disappearing letters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 pygame.init()
 
@@ -6,6 +7,21 @@ pygame.init()
 WIDTH, HEIGHT = 800, 500
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman Game")
+
+# Button Variables
+RADIUS = 20
+GAP = 15
+letters = []
+A = 65
+startx = round((WIDTH - (RADIUS*2 + GAP) * 13) /2)
+starty = 400
+for i in range(26):
+    x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13)) 
+    y = starty + ((i // 13) * (GAP + RADIUS * 2))
+    letters.append([x, y, chr(A + i), True])
+
+# Fonts 
+LETTER_FONT = pygame.font.SysFont('arial', 26)
 
 # Load images
 images = []
@@ -18,23 +34,43 @@ hangman_status = 0
 
 # Colors
 WHITE = (255, 255, 255) 
+BLACK = (0, 0, 0) 
 
 # Game Loop
 FPS = 60
 clock = pygame.time.Clock()
 run = True
+
+def draw():
+    win.fill(WHITE)
+
+    # Draw Buttons
+    for letter in letters:
+        x, y, ltr, visible = letter
+        if visible:
+            pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
+            text = LETTER_FONT.render(ltr, 1, BLACK)
+            win.blit(text, (x - text.get_width()/2, y - text.get_height()/2))
+
+    win.blit(images[hangman_status], (150, 100))
+    pygame.display.update()
+
+
 while run:
     clock.tick(FPS)
 
-    win.fill(WHITE)
-    win.blit(images[hangman_status], (150, 100))
-    pygame.display.update()
+    draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            print(pos)
+            m_x, m_y = pygame.mouse.get_pos()
+            for letter in letters:
+                x, y, ltr, visible = letter
+                if visible:
+                    dis = math.sqrt((x - m_x)**2 + (y - m_y)**2)
+                    if dis < RADIUS:
+                        letter[3] = False
 
 pygame.quit()
